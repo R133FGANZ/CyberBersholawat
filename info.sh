f2a4c319709c90f5dba3434e1de6ce7b3273569d
R133FGANZ <50979010+SCTcyber@users.noreply.github.com>
2019-09-01 19:42:21 +0700

    Add files via upload

---
+++ info.sh
@@ -0,0 +1,9 @@
+clear
+
+toilet -f slant --metal "Info Tools"
+
+echo "Gunakan Tools ini dgn bijak"
+echo "\033[1;33mkami membuat tools ini untuk mengajak kalian bertaubat"
+echo "Bertaubat kepada Allah SWT"
+echo "\033[1;32mMakasih sudah bersholawat dengan kamis"
+sleep 7
